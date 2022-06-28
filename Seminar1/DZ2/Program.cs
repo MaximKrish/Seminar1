47563f2106862f315c382dc8748acfd35ebdd0d0
Maxim <m.krishnevskiy@kmg.kz>
2022-06-28 18:05:03 +0600

    Seminar1 DZ2

---
+++ Seminar1/DZ2/Program.cs
@@ -0,0 +1,9 @@
+﻿int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if (numberB > max) {
+    Console.Write("max = ");
+ Console.Write(numberB);
+} 
+else Console.Write("max = ");
+Console.Write(max);
