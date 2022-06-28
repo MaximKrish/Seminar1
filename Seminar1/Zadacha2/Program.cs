ae14afd14693da1f812b331aaf22a4b442ddd7ff
Maxim <m.krishnevskiy@kmg.kz>
2022-06-28 12:56:28 +0600

    Zadacha 2

--- Seminar1/Zadacha2/Program.cs
+++ Seminar1/Zadacha2/Program.cs
@@ -1,1 +1,4 @@
-﻿//efsdafsdds
+﻿int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberB*numberB == numberA) Console.Write("YES");
+else Console.Write("NO");
